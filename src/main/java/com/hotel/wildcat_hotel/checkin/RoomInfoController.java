1bbea54fe4e114031f67188348002eca845573a7
Shyyeb <shyyeb.santillan@cit.edu>
2026-04-30 19:45:01 +0800

    Implemented CheckOutController logic and UI IDs

--- src/main/java/com/hotel/wildcat_hotel/checkin/RoomInfoController.java
+++ src/main/java/com/hotel/wildcat_hotel/checkin/RoomInfoController.java
@@ -1,5 +1,21 @@
 package com.hotel.wildcat_hotel.checkin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 public class RoomInfoController {
-    
+
+    @FXML private Label lblRoomNumber;
+    @FXML private Label lblRoomType;
+    @FXML private Label lblPrice;
+    @FXML private Label lblStatus;
+    @FXML private Button btnConfirmSelection;
+
+    @FXML
+    void handleConfirmSelection(ActionEvent event) {
+        // This is where we will pass the room data to the next screen
+        System.out.println("Room selected!");
+    }
 }
