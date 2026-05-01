b7e75faf163e31e611991c90ea34b2ae236121ac
Gian-Caparas <gianjoebert.caparas@cit.edu>
2026-05-01 23:07:02 +0800

    Added GUI, and the system is 85% done

--- src/main/java/com/hotel/wildcat_hotel/checkout/CheckOutController.java
+++ src/main/java/com/hotel/wildcat_hotel/checkout/CheckOutController.java
@@ -1,5 +1,23 @@
 package com.hotel.wildcat_hotel.checkout;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 public class CheckOutController {
-    
+
+    @FXML private TextField txtBookingId;
+    @FXML private Label lblGuestName;
+    @FXML private Label lblTotalAmount;
+    @FXML private Button btnProcessCheckOut;
+
+    @FXML
+    void handleCheckOut(ActionEvent event) {
+        //It will get the Booking ID from the text field
+        //It will search your database for that guest
+        //It will update the room status in MySQL to 'Available'
+        System.out.println("Processing check-out for ID: " + txtBookingId.getText());
+    }
 }
