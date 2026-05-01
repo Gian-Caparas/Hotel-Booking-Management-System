5a58686fc8e353e6a671613a273d03595eb6293d
Shyyeb <shyyeb.santillan@cit.edu>
2026-05-01 12:23:54 +0800

    Added RoomsController base code

--- src/main/java/com/hotel/wildcat_hotel/rooms/RoomsController.java
+++ src/main/java/com/hotel/wildcat_hotel/rooms/RoomsController.java
@@ -1,5 +1,22 @@
 package com.hotel.wildcat_hotel.rooms;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.event.ActionEvent;
+
 public class RoomsController {
-    
+
+    @FXML private TableView<?> tblRooms;
+    @FXML private TableColumn<?, ?> colRoomNumber;
+    @FXML private TableColumn<?, ?> colType;
+    @FXML private TableColumn<?, ?> colStatus;
+    @FXML private Button btnRefresh;
+
+    @FXML
+    void handleRefresh(ActionEvent event) {
+       //triggers the reload data of MySQL hotel_db
+        System.out.println("Refreshing room list....");
+    }
 }
